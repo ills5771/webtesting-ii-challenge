7afe0dcc2aec3c0fba47ec50024fe64d3d5ddc3f
ills5771 <ilyayelizarov1@gmail.com>
2019-04-09 12:33:53 -0700

    initial commit, adds create react-app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h2>Welcome</h2>
+      </>
+    );
+  }
+}
+
+export default App;
